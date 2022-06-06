b9294021540b01d348bc1acebd86b9587194582d
MATHI <76192299+mathisudar@users.noreply.github.com>
2022-06-06 09:54:02 +0530

    Create 0006_AnimateCircle.js

---
+++ CodeTrain/0006_AnimateCircle.js
@@ -0,0 +1,20 @@
+https://editor.p5js.org/mathisudar2007/sketches/4r0AnesqF
+
+
+function setup() {
+  createCanvas(400, 400);
+  background(0,255,0);
+}
+
+function draw() {
+  
+  // background(0);
+  noStroke();
+  fill(255);
+  circle(mouseX,mouseY,20);
+  
+}
+
+function mousePressed(){
+  background(0);
+}
