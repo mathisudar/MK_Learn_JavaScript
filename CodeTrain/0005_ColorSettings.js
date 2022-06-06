a8eb70b0d63a864a00bdd477d8ac86f96d744f3e
MATHI <76192299+mathisudar@users.noreply.github.com>
2022-06-06 09:52:06 +0530

    Create 0005_ColorSettings.js

---
+++ CodeTrain/0005_ColorSettings.js
@@ -0,0 +1,21 @@
+https://editor.p5js.org/mathisudar2007/full/gp_TEaUlR
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  
+  rectMode(CENTER);
+  fill(0,0,255);
+  stroke(0,255,0);
+  rect(200,150,150,150);
+  
+  fill(255,0,0,150);
+  stroke(255);
+  strokeWeight(8)
+  noStroke();
+  ellipse(150,250,100,75)
+  
+}
