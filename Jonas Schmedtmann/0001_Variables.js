f9c3af5619c17381b2f07a1f4fa77222dbde8c9d
MATHI <76192299+mathisudar@users.noreply.github.com>
2022-06-08 18:11:57 +0530

    Create 0001_Variables.js

---
+++ Jonas Schmedtmann/0001_Variables.js
@@ -0,0 +1,26 @@
+let x = "Hello World!";
+let firstName = "Mathi";
+let lastName = "Krishnan";
+
+let country = 'INDIA';
+let continent = 'ASIA';
+let population = 200;
+
+let isIsland = false;
+let language = "Hindi";
+
+ 
+
+console.log("\n");
+console.log(x);
+console.log(firstName+" "+lastName+"\n");
+
+console.log("My Country:" + country);
+console.log("My Continent:" + continent);
+console.log("Population:" + population+"\n");
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language+"\n");
+ 
